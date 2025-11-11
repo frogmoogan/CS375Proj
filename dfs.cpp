b804e01e93952403084bb565462bafacab3df268
ldonald1 <lelad12550@gmail.com>
2025-11-11 13:01:30 -0500

    dfs

---
+++ dfs.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Graph {
+
+    private:
+    vector<vector<int>> g;// adjacency matrix
+    vector<int> color; // color
+    vector<int> parent; // parent
+
+    public:
+
+    Graph(){
+        
+
+    }
+
+
+
+
+
+};
+
+
+int main(){
+
+
+
+    return 0;
+}
